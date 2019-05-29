295595abc6ad8533d9455d9b33c6669dee0448c5
Alvin <alvin2304@gmail.com>
2019-05-29 14:02:10 +0700

    delete unused line

--- index.js
+++ index.js
@@ -7,23 +7,6 @@ const YAML = require("yamljs");
 app.set("view engine", "pug");
 app.use(express.static(__dirname + "/public"));
 
-// fs.readdir("/data/fazzcard-backend/docs", (error, items) => {
-//   app.get("/", (req, res) => {
-//     res.render("main", {
-//       title: "Route files",
-//       items: items.map(item => {
-//         return {
-//           title: item,
-//           path: item.replace(/\.[^/.]+$/, "")
-//         };
-//       })
-//     });
-//   });
-//   items.forEach(val => {
-//     var path = val.replace(".yaml", "");
-    
-//   });
-// });
 app.use(`/`, swaggerUi.serve, (req, res) => {
     swaggerUi.setup(YAML.load(`/data/swagger.yaml`))(req, res);
   });
--- start.sh
+++ start.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# index=0
-# pid_array=()
-# for entry in ./api/*
-# do
-#     echo "$entry"
-#     apisprout -p $(( 5000 + index )) $entry &
-#     pid_array+=" $!"
-#     index=$(( index + 1 ))
-# done
-# function finish {
-#     for id in $pid_array
-#     do
-#         kill "$id"
-#     done
-# }
 apisprout -p 5000 /data/swagger.yaml &
 function finish {
     kill "$!"
