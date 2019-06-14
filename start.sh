24f669ee309e009795e81bd6ea824b2d2c636c8e
Alvin <alvin2304@gmail.com>
2019-06-14 13:27:53 +0700

    read swagger file from env

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-apisprout -p 5000 /usr/share/nginx/html/swagger.yaml &
+apisprout -p 5000 /usr/share/nginx/html/${SPEC_URL} &
+chmod a+x /usr/local/bin/docker-run.sh
+/usr/local/bin/docker-run.sh
+
 function finish {
     kill "$!"
 }
 trap finish EXIT
-
-chmod a+x /usr/local/bin/docker-run.sh
-/usr/local/bin/docker-run.sh
